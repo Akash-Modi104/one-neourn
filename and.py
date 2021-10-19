bba823b23875676267e572aa36288af678dc5614
akash <developer.akash104@gmail.com>
2021-10-19 18:24:48 +0530

    tqdm, lodding readme

--- and.py
+++ and.py
@@ -1,3 +1,9 @@
+'''
+author: akash
+email : developer.akash104@gmail.com
+'''
+
+
 from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
